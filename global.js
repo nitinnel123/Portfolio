d17e80fa41ee5e0c39a346ee15760370461ae1e5
Nitin <nnellisserry@ucsd.edu>
2025-10-16 23:04:07 -0700

    update

--- global.js
+++ global.js
@@ -4,10 +4,14 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a"); 
+const navLinks = $$("nav a");
 
 const currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
+  (a) =>
+    a.host === location.host &&
+    (location.pathname === a.pathname ||
+     location.pathname === a.pathname + "index.html" ||
+     location.pathname.startsWith(a.pathname))
 );
 
 currentLink?.classList.add("current");
