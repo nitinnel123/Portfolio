ba0ca55a1aeef822377663903e6a66f30952cd81
Nitin <nnellisserry@ucsd.edu>
2025-10-25 16:57:11 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,32 +1,20 @@
 import { fetchJSON, renderProjects } from "../global.js";
 
 async function initProjects() {
+
   const projects = await fetchJSON("../lib/projects.json");
   const container = document.querySelector(".projects");
   renderProjects(projects, container, "h2");
+
 
   const title = document.querySelector(".projects-title");
   if (title) {
     title.textContent = `Projects (${projects.length})`;
   }
-}
-
-initProjects();
-
-import { fetchJSON, renderProjects } from "../global.js";
-
-async function initProjects() {
-  const projects = await fetchJSON("../lib/projects.json");
-
-  const container = document.querySelector(".projects");
-  renderProjects(projects, container, "h2");
-
-  const title = document.querySelector(".projects-title");
-  if (title) title.textContent = `Projects (${projects.length})`;
 
 
   try {
-    const username = "nitinnel123"; 
+    const username = "nitinnel123";
     const url = `https://api.github.com/users/${username}/repos?sort=updated&per_page=5`;
 
     const githubRepos = await fetchJSON(url);
@@ -34,10 +22,11 @@ async function initProjects() {
 
     const githubProjects = githubRepos.map((repo) => ({
       title: repo.name,
-      image: "../images/github.svg", 
+      image: "../images/github.svg",
       description: repo.description || "No description available.",
       url: repo.html_url,
     }));
+
 
     githubProjects.forEach((proj) => {
       const article = document.createElement("article");
