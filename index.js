67519aa399b4cf870504c05b22b9ad7328c15782
Nitin <nnellisserry@ucsd.edu>
2025-10-25 17:57:46 -0700

    update

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ async function showLatestProjects() {
 }
 
 showLatestProjects();
+
+import { fetchGitHubData } from "./global.js";
+
+async function init() {
+  const username = "nitinnel123"; 
+  const githubData = await fetchGitHubData(username);
+
+  console.log("Fetched GitHub Data:", githubData);
+
+  const profileStats = document.querySelector("#profile-stats");
+  if (profileStats) {
+    profileStats.innerHTML = `
+      <h2>GitHub Profile Stats</h2>
+      <dl class="stats-grid">
+        <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+        <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+        <dt>Followers:</dt><dd>${githubData.followers}</dd>
+        <dt>Following:</dt><dd>${githubData.following}</dd>
+      </dl>
+    `;
+  }
+}
+
+init();
