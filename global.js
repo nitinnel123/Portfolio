5516f4005929d9bf2b441c1ef7ab125c5983b55b
Nitin <nnellisserry@ucsd.edu>
2025-11-08 13:19:05 -0800

    updates

--- global.js
+++ global.js
@@ -23,6 +23,7 @@ const pages = [
   { url: "projects/", title: "Projects" },
   { url: "cv/", title: "CV" },
   { url: "contact/", title: "Contact" },
+  { url: "meta/", title: "Meta" },
   { url: "https://github.com/nitinnel123", title: "GitHub" },
 ];
 
