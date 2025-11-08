82366622166375f24f8aaa1a1b176980807ea6bd
Nitin <nnellisserry@ucsd.edu>
2025-11-08 14:56:11 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -46,6 +46,64 @@ function renderCommitInfo(data, commits) {
   dl.append("dt").text("Total commits");
   dl.append("dd").text(commits.length);
 
+  dl.append("dt").text("Number of files");
+  dl.append("dd").text(d3.group(data, (d) => d.file).size);
+
+  dl.append("dt").text("Average file depth");
+  dl.append("dd").text(d3.mean(data, (d) => d.depth).toFixed(2));
 }
 
+
 renderCommitInfo(data, commits);
+
+function computeStats(data) {
+  
+  const fileLengths = d3.rollups(
+    data,
+    (v) => d3.max(v, (d) => d.line),  
+    (d) => d.file
+  );
+
+  const averageFileLength = d3.mean(fileLengths, (d) => d[1]);
+
+  
+  const deepestFile = d3.greatest(data, (d) => d.depth);
+
+  
+  const longestLine = d3.greatest(data, (d) => d.length);
+
+  
+  const workByPeriod = d3.rollups(
+    data,
+    (v) => v.length, 
+    (d) => new Date(d.datetime).toLocaleString("en", { dayPeriod: "short" })
+  );
+
+  const busiestPeriod = d3.greatest(workByPeriod, (d) => d[1]);
+
+  return {
+    averageFileLength,
+    deepestFile,
+    longestLine,
+    busiestPeriod,
+  };
+}
+
+function renderQuantitativeStats(stats) {
+  const dl = d3.select("#stats").append("dl").attr("class", "stats");
+
+  dl.append("dt").text("Average file length");
+  dl.append("dd").text(stats.averageFileLength.toFixed(2));
+
+  dl.append("dt").text("Deepest file");
+  dl.append("dd").text(stats.deepestFile.file);
+
+  dl.append("dt").text("Longest line (chars)");
+  dl.append("dd").text(stats.longestLine.length);
+
+  dl.append("dt").text("Busiest time of day");
+  dl.append("dd").text(stats.busiestPeriod[0]);
+}
+
+const stats = computeStats(data);
+renderQuantitativeStats(stats);
